35a1f13e8f7f1ab0db3e42f74f9e3b33beeb8079
AkkoLeTaco <Liciaga.Angelo@live.jobcorps.org>
2022-05-06 19:27:56 +0000

    add signup settings

--- src/api/routes.py
+++ src/api/routes.py
@@ -16,3 +16,17 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+@api.route('/signup', methods=['GET'])
+def get_user():
+     user = User.query.all()
+     user_list = list(map(lambda x: x.serialize(), user))
+     return jsonify(user_list), 200
+
+@api.route('/signup', methods=['POST'])
+def create_user():
+    response_body = request.get._json()
+    new_user = User(email=response_body['email'], password=response_body['password'])
+    db.session.add(new_user)
+    db.session.commit()
+    return "success", 200
